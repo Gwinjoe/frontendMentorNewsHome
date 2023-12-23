d5bd5e20de18fd065abe6f9a678e36f34fe5e4ee
Gwinjoe <123786736+Gwinjoe@users.noreply.github.com>
2023-12-23 00:24:55 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,7 @@ const navbar = document.querySelector('#navbar');
 const navclosebtn = document.querySelector('#navclosebutton');
 const web3img = document.querySelector('.web-3')
 
- if (window.innerWidth > 900px){
-     navbar.classList.remove('hidden');
-  }
-
+ 
 window.addEventListener('load', function() {
  
   AOS.init();
