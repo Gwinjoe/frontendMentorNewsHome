e53f905a2078b85c0e38b48082823eac7324aecd
Gwinjoe <123786736+Gwinjoe@users.noreply.github.com>
2023-12-24 14:21:07 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,32 +6,18 @@ const web3img = document.querySelector('.web-3');
 
 
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    navbar.classList.remove('hidden');
-}
 
-// Create a MediaQueryList object
-var x = window.matchMedia("(max-width: 900px)")
-
-// Call listener function at run time
-myFunction(x);
-
-// Attach listener function on state changes
-x.addEventListener("change", function() {
-  myFunction(x);
-});
  
 window.addEventListener('load', function() {
  
   AOS.init();
   navbtn.addEventListener('click', function () {
   navbar.classList.remove('hidden');
-})
+});
 
 
 navclosebtn.addEventListener('click', function() {
   navbar.classList.add('hidden');
-})
+});
   
-})
+});
